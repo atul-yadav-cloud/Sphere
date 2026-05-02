c384ef9186c1747f059877fe752394b74690dfff
Atul Pratap Yadav <apyadav7379@gmail.com>
2026-05-02 20:28:07 +0530

    final commit

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"http://sphereapp.netlify.app",  // http://localhost:5173 change star with this
+        origin:"https://sphereapp.netlify.app",  // http://localhost:5173 change star with this
         methods:["GET","POST"]
     }
 })
